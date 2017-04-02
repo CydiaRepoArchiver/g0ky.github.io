b6de5b121cfe1e17e4c137620d7ea02cc4cd224e
g0ky <goky08@outlook.es>
2017-04-02 14:26:18 -0600

    update

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -9,8 +9,8 @@ function iOSversion() {
 }
 ver = iOSversion();
 if (ver[0] >= 8) {
-	document.getElementById('version').innerHTML='<p><strong>✓</strong> Your device is supported.</p>';
+	document.getElementById('version').innerHTML='<p><strong>✓</strong> Your device is supported 😁.</p>';
 }
 else {
-	document.getElementById('version').innerHTML='<p><strong>x</strong> Your device is not supported.</p>';
+	document.getElementById('version').innerHTML='<p><strong>x</strong> Your device is not supported🙁.</p>';
 }
