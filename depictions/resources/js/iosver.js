006ea949b05f4acc7aecec0e77c3a3145dee29c6
g0ky <goky08@outlook.es>
2017-04-09 15:25:45 -0600

    update

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -8,7 +8,7 @@ function iOSversion() {
 	}
 }
 ver = iOSversion();
-if (ver[0] >= 8) {
+if (ver[0] >= 7) {
 	document.getElementById('version').innerHTML='<p><strong>✓</strong> Your device is supported 😁</p>';
 }
 else {
